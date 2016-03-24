04c4096ce7e2965363b8148e418f823c4926de75
Piotr Kosiński <Piotr.Kosinski@powel.com>
2016-03-24 09:56:19 +0100

    Finding the best matching value from the range

--- src/zip_with/core.clj
+++ src/zip_with/core.clj
@@ -1,8 +1,19 @@
 (ns zip-with.core)
 
-(defn- in-coll [coll pred val]
-  [val (first (filter (partial pred val) coll))]
+(defn- abs [n] (max n (- n)))
+
+(defn- deltas [val1 val2]
+  [val2 (abs (- val1 val2))]
   )
+
+(defn- in-coll [coll pred val]
+  (let [
+    in-range (filter (partial pred val) coll)
+    diffs (map (partial deltas val) in-range)
+    best-match (ffirst (sort-by last diffs))
+    ]
+  [val best-match]
+  ))
 
 (defn zip-with [coll1 coll2 pred]
   (map (partial in-coll coll2 pred) coll1)
--- test/zip_with/core_test.clj
+++ test/zip_with/core_test.clj
@@ -19,3 +19,21 @@
     ]
   (testing "Checking first five elements from result"
     (is (= first-five expected-five)))))
+
+  (deftest b-test
+      (let [
+        coll1 [1 2 3 4 5]
+        coll2 [0.1 0.2 0.5 0.8 1.2 1.5 1.8 3.3 5]
+        delta 0.5
+        pred (fn [val1 val2]
+          (let [
+            r1 (- val2 delta)
+            r2 (+ val2 delta)
+            ]
+            (and (>= val1 r1) (<= val1 r2))))
+        result (zip-with coll1 coll2 pred)
+        first-five (take 5 result)
+        expected-five [[1 0.8] [2 1.8] [3 3.3] [4 nil] [5 5]]
+        ]
+      (testing "Checking first five elements from result"
+        (is (= first-five expected-five)))))
