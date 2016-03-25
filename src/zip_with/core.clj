cf7670355462922576ec72472b664f9f7c543be3
Piotr Kosiński <Piotr.Kosinski@powel.com>
2016-03-25 10:30:10 +0100

    Using distance fn directly in sort-by

--- src/zip_with/core.clj
+++ src/zip_with/core.clj
@@ -2,15 +2,10 @@
 
 (defn- abs [n] (max n (- n)))
 
-(defn- deltas [val1 val2]
-  [val2 (abs (- val1 val2))]
-  )
-
 (defn- in-coll [coll pred val]
   (let [
     in-range (filter (partial pred val) coll)
-    diffs (map (partial deltas val) in-range)
-    best-match (ffirst (sort-by last diffs))
+    best-match (first (sort-by #(abs (- % val)) in-range))
     ]
   [val best-match]
   ))
--- test/zip_with/core_test.clj
+++ test/zip_with/core_test.clj
@@ -2,38 +2,17 @@
   (:require [clojure.test :refer :all]
             [zip-with.core :refer :all]))
 
-(deftest a-test
-  (let [
-    coll1 (range 0 1000 10)
-    coll2 (range 0 1000 20)
-    delta 5
-    pred (fn [val1 val2]
-      (let [
-        r1 (- val2 delta)
-        r2 (+ val2 delta)
-        ]
-        (and (>= val1 r1) (<= val1 r2))))
-    result (zip-with coll1 coll2 pred)
-    first-five (take 5 result)
-    expected-five [[0 0] [10 nil] [20 20] [30 nil] [40 40]]
-    ]
-  (testing "Checking first five elements from result"
-    (is (= first-five expected-five)))))
+(defn- abs [n] (max n (- n)))
 
-  (deftest b-test
-      (let [
-        coll1 [1 2 3 4 5]
-        coll2 [0.1 0.2 0.5 0.8 1.2 1.5 1.8 3.3 5]
-        delta 0.5
-        pred (fn [val1 val2]
-          (let [
-            r1 (- val2 delta)
-            r2 (+ val2 delta)
-            ]
-            (and (>= val1 r1) (<= val1 r2))))
-        result (zip-with coll1 coll2 pred)
-        first-five (take 5 result)
-        expected-five [[1 0.8] [2 1.8] [3 3.3] [4 nil] [5 5]]
-        ]
-      (testing "Checking first five elements from result"
-        (is (= first-five expected-five)))))
+(deftest b-test
+    (let [
+      coll1 [1 2 3 4 5]
+      coll2 [0.1 0.2 0.5 0.8 1.2 1.5 1.8 3.3 5]
+      delta 0.5
+      pred (fn [val1 val2] (<= (abs (- val1 val2)) delta))
+      result (zip-with coll1 coll2 pred)
+      first-five (take 5 result)
+      expected-five [[1 0.8] [2 1.8] [3 3.3] [4 nil] [5 5]]
+      ]
+    (testing "Checking first five elements from result"
+      (is (= first-five expected-five)))))
