a89acd2e40c6da6a5ca60799d36f1f1b5dbd1a0f
kosecki123 <piotrkosinski@gmail.com>
2016-03-31 19:54:31 +0200

    project.clj description, version and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject zip-with "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject zip-with "1.0.0"
+  :description "A Clojure library designed to zip two collections by using predicate for matching"
+  :url "http://github.com/kosecki123/zip-with"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
