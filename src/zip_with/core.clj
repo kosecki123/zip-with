08d195ca2061c5d6938ec3670f70d8f83e80a8a4
kosecki123 <piotrkosinski@gmail.com>
2016-03-31 19:49:36 +0200

    "Code reformat with cljfmt"

--- src/zip_with/core.clj
+++ src/zip_with/core.clj
@@ -3,13 +3,9 @@
 (defn- abs [n] (max n (- n)))
 
 (defn- in-coll [coll pred val]
-  (let [
-    in-range (filter (partial pred val) coll)
-    best-match (first (sort-by #(abs (- % val)) in-range))
-    ]
-  [val best-match]
-  ))
+  (let [in-range (filter (partial pred val) coll)
+        best-match (first (sort-by #(abs (- % val)) in-range))]
+    [val best-match]))
 
 (defn zip-with [coll1 coll2 pred]
-  (map (partial in-coll coll2 pred) coll1)
-  )
+  (map (partial in-coll coll2 pred) coll1))
--- test/zip_with/core_test.clj
+++ test/zip_with/core_test.clj
@@ -5,14 +5,12 @@
 (defn- abs [n] (max n (- n)))
 
 (deftest b-test
-    (let [
-      coll1 [1 2 3 4 5]
-      coll2 [0.1 0.2 0.5 0.8 1.2 1.5 1.8 3.3 5]
-      delta 0.5
-      pred (fn [val1 val2] (<= (abs (- val1 val2)) delta))
-      result (zip-with coll1 coll2 pred)
-      first-five (take 5 result)
-      expected-five [[1 0.8] [2 1.8] [3 3.3] [4 nil] [5 5]]
-      ]
+  (let [coll1 [1 2 3 4 5]
+        coll2 [0.1 0.2 0.5 0.8 1.2 1.5 1.8 3.3 5]
+        delta 0.5
+        pred (fn [val1 val2] (<= (abs (- val1 val2)) delta))
+        result (zip-with coll1 coll2 pred)
+        first-five (take 5 result)
+        expected-five [[1 0.8] [2 1.8] [3 3.3] [4 nil] [5 5]]]
     (testing "Checking first five elements from result"
       (is (= first-five expected-five)))))
